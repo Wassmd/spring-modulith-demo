86b4e36c717dc3396c6ce03b080e1316904a7103
Wasim <mohammed.wasimuddin@rewe-group.com>
2025-10-24 12:12:12 +0200

    Add Modulith annotation

--- src/main/java/com/paxier/spring_modulith_demo/SpringModulithDemoApplication.java
+++ src/main/java/com/paxier/spring_modulith_demo/SpringModulithDemoApplication.java
@@ -2,8 +2,10 @@ package com.paxier.spring_modulith_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.modulith.Modulith;
 
 @SpringBootApplication
+@Modulith
 public class SpringModulithDemoApplication {
 
 	public static void main(String[] args) {
